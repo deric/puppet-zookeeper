26b7a85788eba8f64277260b584c3ee999cc5f63
Cam Parry <cam.parry@gmail.com>
2015-01-13 17:14:51 +0000

    Add support for package install for Redhat

--- manifests/os/redhat.pp
+++ manifests/os/redhat.pp
@@ -6,7 +6,10 @@ class zookeeper::os::redhat(
   $cleanup_sh        = '/usr/lib/zookeeper/bin/zkCleanup.sh',
   $datastore         = '/var/lib/zookeeper',
   $user              = 'zookeeper',
+  $start_with        = 'init.d',
   $ensure_cron       = true,
+  # cloudera package is called zookeeper-server
+  $service_package   = 'zookeeper-server',
   $packages          = ['zookeeper'],
   $manual_clean      = false
 ) {
@@ -14,6 +17,18 @@ class zookeeper::os::redhat(
   # allow installing multiple packages, like zookeeper, zookeeper-bin etc.
   ensure_resource('package', $packages, {'ensure' => $ensure})
 
+  if ($start_with == 'init.d') {
+    package { [$service_package]: #init.d scripts for zookeeper
+      ensure  => $ensure,
+      require => Package['zookeeper']
+    }
+  }
+
+  package { 'cloudera-cdh':
+    ensure => 'installed',
+    source => 'http://archive.cloudera.com/cdh4/one-click-install/redhat/6/x86_64/cloudera-cdh-4-0.x86_64.rpm',
+    provider => 'rpm'
+  }
 
   # if !$cleanup_count, then ensure this cron is absent.
   if ($manual_clean and $snap_retain_count > 0 and $ensure != 'absent') {
