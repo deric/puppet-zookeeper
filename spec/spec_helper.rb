566eac8499e6a2f76503467a41b9f1cf44483a47
Tomas Barton <barton.tomas@gmail.com>
2016-05-27 11:08:14 +0200

    remove deprecated rspec setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@ require 'rspec-puppet/coverage'
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
-  c.treat_symbols_as_metadata_keys_with_true_values = true
   c.include PuppetlabsSpec::Files
   c.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
 
