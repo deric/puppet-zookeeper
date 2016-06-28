c4a1c817ab182698fa8e4c366d40325e7bf31dfe
Tomas Barton <barton.tomas@gmail.com>
2016-06-28 15:18:04 +0200

    simplify: installation order should be solved from main manifest

--- manifests/sasl.pp
+++ manifests/sasl.pp
@@ -14,7 +14,6 @@ class zookeeper::sasl(
   $use_ticket_cache = false,
   $realm            = $::domain,
   $principal        = "zookeeper/${::fqdn}",
-  $service_name     = $::zookeeper::params::service_name,
 ) inherits ::zookeeper::params {
 
   file{"${cfg_dir}/jaas.conf":
@@ -22,7 +21,6 @@ class zookeeper::sasl(
     owner   => $user,
     group   => $group,
     content => template('zookeeper/conf/jaas.conf.erb'),
-    before  => Service[$service_name],
   }
 
   file{"${cfg_dir}/java.env":
@@ -30,7 +28,6 @@ class zookeeper::sasl(
     owner   => $user,
     group   => $group,
     content => template('zookeeper/conf/java.env.erb'),
-    before  => Service[$service_name],
   }
 }
 
