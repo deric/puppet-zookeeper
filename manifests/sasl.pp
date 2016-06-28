572bb06247027c5d31f088a5caca36ea2ef07e3c
Tomas Barton <barton.tomas@gmail.com>
2016-06-28 14:46:18 +0200

    fetch user and group from params

--- manifests/sasl.pp
+++ manifests/sasl.pp
@@ -2,22 +2,17 @@
 #
 # This module manages Zookeeper sasl auth
 #
-# Parameters: None
-#
-# Actions: None
-#
-#
 # Should not be included directly
 #
 class zookeeper::sasl(
   $use_keytab       = true,
   $cfg_dir          = '/etc/zookeeper/conf',
-  $user             = 'zookeeper',
-  $group            = 'zookeeper',
+  $user             = $::zookeeper::params::user,
+  $group            = $::zookeeper::params::group,
   $keytab_path      = '/etc/zookeeper/conf/zookeeper.keytab',
   $store_key        = true,
   $use_ticket_cache = false,
-  $realm            = "${::domain}",
+  $realm            = $::domain,
   $principal        = "zookeeper/${::fqdn}",
 ) {
 
@@ -28,7 +23,7 @@ class zookeeper::sasl(
     content => template('zookeeper/conf/jaas.conf.erb'),
     before  => Class['zookeeper::service'],
   }
-  
+
   file{"${cfg_dir}/java.env":
     ensure  => present,
     owner   => $user,
