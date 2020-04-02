5531ae082df53789a2a74c04fcc1796335f18de0
Dennis Hoppe <github@debian-solutions.de>
2020-04-02 11:37:57 +0200

    Remove check for supported operating systems

--- spec/acceptance/zookeeper_spec.rb
+++ spec/acceptance/zookeeper_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper_acceptance'
 
-describe 'zookeeper defintion', unless: UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) do
+describe 'zookeeper defintion' do
   context 'basic setup' do
     it 'install zookeeper' do
       pp = <<-EOS
