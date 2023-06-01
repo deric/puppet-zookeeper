23f8ffc1672f7ae74475336ddf35aa68b01dc3bb
Tomas Barton <barton.tomas@gmail.com>
2023-06-01 10:46:52 +0200

    Configure acceptance tests (#192)
    Include basic acceptance tests in pipeline.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,36 +1,3 @@
-require 'beaker-rspec'
-require 'beaker-puppet'
-require 'beaker/puppet_install_helper'
-require 'beaker/module_install_helper'
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-install_ca_certs unless %r{pe}i.match?(ENV['PUPPET_INSTALL_TYPE'])
-install_module_on(hosts)
-install_module_dependencies_on(hosts)
-
-RSpec.configure do |c|
-  # Readable test descriptions
-  c.formatter = :documentation
-  hosts.each do |host|
-    if fact_on(host, 'osfamily') == 'Debian'
-      on host, puppet('resource', 'package', 'net-tools', 'ensure=installed')
-      on host, puppet('resource', 'package', 'netcat', 'ensure=installed')
-    end
-    if fact_on(host, 'osfamily') == 'RedHat'
-      case fact('os.release.major')
-      when '6'
-        on host, puppet('resource', 'package', 'nc', 'ensure=installed')
-      when '7'
-        on host, puppet('resource', 'package', 'net-tools', 'ensure=installed')
-        on host, puppet('resource', 'package', 'nmap-ncat', 'ensure=installed')
-      end
-    end
-    if fact_on(host, 'osfamily') == 'Suse'
-      on host, puppet('resource', 'package', 'net-tools', 'ensure=installed')
-      on host, puppet('resource', 'package', 'netcat-openbsd', 'ensure=installed')
-    end
-    if host[:platform].include?('el-7-x86_64') && host[:hypervisor].include?('docker')
-      on(host, "sed -i '/nodocs/d' /etc/yum.conf")
-    end
-  end
-end
+configure_beaker
